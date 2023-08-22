f665379c3bb781c401208cfb236b6097a2aab103
cityhallin <cityhallin.com>
2023-08-22 17:37:37 -0600

    initial creation

---
+++ resources/azure/automation_account/stop_vms_by_tag.ps1
@@ -0,0 +1,69 @@
+
+<#
+    .SYNOPSIS
+    Stop Azure VMs based on tag value
+
+    .DESCRIPTION
+    This PowerShell script is meant to run inside an Azure Automation Account Runbook that will
+    search for Azure VMs with a specific tag value and stop them. 
+    Automation Accounts are automatically created with Az PowerShell modules pre-installed. 
+
+    ----Requirements----
+    - Run inside Azure Automation Account Runbook
+    - The Azure Automation Account must have a System-Managed ID attached
+    - The System-Managed ID must have a proper Azure role assignment to impact needed Azure VMs
+           
+#>
+
+#Tag name and value script will search for on VMs
+$tagName = "shutdown"
+$tagValue = "6pm"
+
+#Ensures you do not inherit an AzContext in your runbook
+Disable-AzContextAutosave -Scope Process | Out-Null
+
+#Connect to Azure with system-assigned managed ID
+Write-Output "Logging in with Managed ID"
+$context = (Connect-AzAccount -Identity).Context
+If ($null -eq $($context.Subscription.name)) {
+    throw (Write-Error "Automation Account Managed ID could not log into Azure. Runbook stopped")
+    exit
+}
+Else {
+    Write-Output "Logged in successfully. Subscription: $($context.Subscription.name)"
+}
+
+#Get VMs with specific tag name and value
+Write-Output "Query VMs with tag=$tagName and value=$tagValue"
+$vms = Get-AzVM -Status | Where-Object { $_.Tags[$tagName] -eq $tagValue }
+If ($vms.count -lt 1) {
+    Write-Output "No VMs found with tag=$tagName and value=$tagValue. Runbook stopped"   
+    exit
+}
+Else {
+    Write-Output "Found $($vms.count) VM(s) with tag=$tagName and value=$tagValue"
+}
+
+#Loop to process power action on select VMs
+$jobFailures = @()
+Write-Output "Processing VMs for power action"
+Foreach ($vm in $vms) {    
+    If (($vm.PowerState -eq "VM running") -or ($vm.PowerState -eq "VM stopped")) {
+        $powerActionRequest = Stop-AzVM -Name $($vm.Name) -ResourceGroupName $($vm.ResourceGroupName) -Force -NoWait #remove -NoWait to force powestate verification before moving to next VM
+        Write-Output "VM=$($vm.Name) Current state=$($vm.PowerState). Sending shutdown signal to VM now"
+    }
+    Elseif ($vm.PowerState -eq "VM deallocated") {            
+        Write-Output "VM=$($vm.Name) Current state=$($vm.PowerState). VM already shutdown. Skipping VM"
+    }
+    Else {              
+        Write-Warning "VM=$($vm.Name) Current state=$($vm.PowerState). Did NOT send shutdown request to VM. Power state incompatibility"
+        $jobFailures += $vm.Name
+    } 
+}
+
+#Output power state action failures
+Write-Output "Failed VM power actions: $($jobFailures.count). Any failures saved to Runbook Warning and Error sections"
+Write-Output $jobFailures
+
+#Runbook finished message
+Write-Output "Runbook Complete"
