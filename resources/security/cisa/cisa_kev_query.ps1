04019bfd6f76a50528ce3971f5bc46a9136fa19b
CityHallin <CityHallin@cityhallin.com>
2024-05-20 15:12:12 -0600

    add cisa kev script

---
+++ resources/security/cisa/cisa_kev_query.ps1
@@ -0,0 +1,35 @@
+﻿
+<#
+    .SYNOPSIS
+    CISA KEV Query
+
+    .DESCRIPTION
+    Simple, example PowerShell script that queries the Known Exploited Vulnerabilities (KEV) Catalog 
+    from the federal Cybersecurity and Infrastructure Security Agency (CISA). It pulls the JSON data
+    from the website and displays in the PowerShell console. https://www.cisa.gov/known-exploited-vulnerabilities-catalog
+
+#>
+
+#URL
+$url = "https://www.cisa.gov/sites/default/files/feeds/known_exploited_vulnerabilities.json"
+
+#Web Request
+$result = Invoke-WebRequest -Uri $url -Method Get
+$kevList = ConvertFrom-Json $result
+
+#Information display examples
+
+    # Latest records
+    $latestRecords = 5
+    $kevList.vulnerabilities `
+        | Sort-Object -Property dateAdded -Descending `
+        | Select-Object -First $latestRecords `
+        | Format-Table cveID,dateadded,vendorProject,product,vulnerabilityName,shortDescription
+
+    #Specific products
+    $productSearch = "chrom"
+    $kevList.vulnerabilities `
+        | Sort-Object -Property dateAdded -Descending `
+        | Where-Object {$_.product -like "*$productSearch*"} `
+        | Format-Table cveID,dateadded,vendorProject,product,vulnerabilityName,shortDescription
+
